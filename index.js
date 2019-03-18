0221686edc87902542b1edd057fa19629dbb615d
Asbjørn Enge <asbjorn@hanafjedle.net>
2019-03-18 15:00:25 +0100

    Basics working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const { exec, spawn } = require('child-process-async')
 const path = require('path')
+const walk = require('walk-promise')
 const args = require('minimist')(process.argv.slice(2), {
   default : {
     contracts : './contracts',
@@ -14,8 +15,15 @@ const image = 'asbjornenge/sliq:1.0.0'
 ;
 
 async function run(cmd) {
-  let compile = `docker run --rm -v ${path.resolve(ctx)}:/ctx ${image} liquidity --no-annot --no-simplify --no-peephole /techel.liq /ctx/tests/basic.liq -o /ctx/tests/basic.techel`
-  let test = `docker run --rm -v ${path.resolve(ctx)}:/ctx ${image} techelson /ctx/tests/basic.techel`
+  let ctxPath = path.resolve(ctx)
+  let contracts = await walk([ctxPath+'/contracts'])
+    .then(c => c.map(_c => _c.root+'/'+_c.name))
+    .then(c => c.map(_c => _c.replace(ctxPath,'/ctx')))
+    .then(c => c.filter(_c => _c.endsWith('liq')))
+    .then(p => p.reduce((fp,p) => fp+' '+p,''))
+  console.log(contracts, contracts.length) 
+  let compile = `docker run --rm -v ${ctxPath}:/ctx ${image} liquidity --no-annot --no-simplify --no-peephole /techel.liq ${contracts} -o /ctx/tests/sliq.techel /ctx/tests/basic.liq`
+  let test = `docker run --rm -v ${ctxPath}:/ctx ${image} techelson /ctx/tests/sliq.techel`
 
   let res_c = await exec(compile)
   let res_t = await exec(test)
