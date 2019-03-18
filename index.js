0ff76334512a55dec582df8d77c6b49f1c31f66a
Asbjørn Enge <asbjorn@hanafjedle.net>
2019-03-18 15:11:03 +0100

    Restruct

--- index.js
+++ index.js
@@ -14,23 +14,39 @@ const image = 'asbjornenge/sliq:1.0.0'
 
 ;
 
-async function run(cmd) {
-  let ctxPath = path.resolve(ctx)
-  let contracts = await walk([ctxPath+'/contracts'])
-    .then(c => c.map(_c => _c.root+'/'+_c.name))
-    .then(c => c.map(_c => _c.replace(ctxPath,'/ctx')))
-    .then(c => c.filter(_c => _c.endsWith('liq')))
-    .then(p => p.reduce((fp,p) => fp+' '+p,''))
-  console.log(contracts, contracts.length) 
-  let compile = `docker run --rm -v ${ctxPath}:/ctx ${image} liquidity --no-annot --no-simplify --no-peephole /techel.liq ${contracts} -o /ctx/tests/sliq.techel /ctx/tests/basic.liq`
+async function run(ctxPath, contracts, testfile) {
+  let compile = `docker run --rm -v ${ctxPath}:/ctx ${image} liquidity --no-annot --no-simplify --no-peephole /techel.liq ${contracts} -o /ctx/tests/sliq.techel ${testfile}`
   let test = `docker run --rm -v ${ctxPath}:/ctx ${image} techelson /ctx/tests/sliq.techel`
-
   let res_c = await exec(compile)
   let res_t = await exec(test)
   return { compile: res_c, test: res_t }
 }
 
+async function getContracts(ctxPath) {
+  let contracts = await walk([ctxPath+'/contracts'])
+    .then(c => c.map(_c => _c.root+'/'+_c.name))
+    .then(c => c.map(_c => _c.replace(ctxPath,'/ctx')))
+    .then(c => c.filter(_c => _c.endsWith('liq')))
+    .then(p => p.reduce((fp,p) => fp+' '+p,''))
+  return contracts
+}
+
+async function getTests(ctxPath) {
+  let tests = await walk([ctxPath+'/tests'])
+    .then(c => c.map(_c => _c.root+'/'+_c.name))
+    .then(c => c.map(_c => _c.replace(ctxPath,'/ctx')))
+    .then(c => c.filter(_c => _c.endsWith('liq')))
+  // TODO if args.only filter
+  return tests
+}
+
 (async () => {
-  let res = await run()
-  console.log(res.test.stdout)
+  let ctxPath = path.resolve(ctx)
+  let contracts = await getContracts(ctxPath)
+  let tests = await getTests(ctxPath)
+  for (let test of tests) {
+    console.log(`===== ${test} =====`)
+    let res = await run(ctxPath, contracts, test)
+    console.log(res.test.stdout)
+  }
 })()
